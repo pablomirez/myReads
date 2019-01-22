112dcd35217dbe0436fda16096d965178698d3d6
pramirez <pramirez@laserspineinstitute.com>
2019-01-22 18:41:40 -0500

    Implemented search using API feature

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import SingleBook from "./SingleBook";
+import * as BooksAPI from "./BooksAPI"
 
 class BookSearch extends Component {
 
     state = {
+        Books: [],
         query: '',
         selectedBook: ''
     }
@@ -13,21 +15,49 @@ class BookSearch extends Component {
         this.setState(() => ({
             query: query.trim()
         }))
+
+        this.searchBook(query)
     }
 
+    searchBook = (query) => {
+
+        if(query.length > 0){
+            BooksAPI.search(query).then((books) => {
+                if(books.length > 0){
+                    books = books.filter((book) => (book.imageLinks))
+                    books = this.setNoneShelf(books)
+                    this.setState(() => {
+                        return { Books: books}
+
+                    })
+                }
+            })
+        }
+
+    }
+
+    setNoneShelf = (books) => {
+        console.log("I'm getting the books to none");
+        console.log(books);
+        //Now I need to set up all this books to have a none shelf
+        for(let book of books){
+            book.shelf = "none"
+        }
+        return books;
+
+    }
 
     onChangeBookFromShelf = (book, shelf) => {
         this.props.changeShelfState(book, shelf);
     }
+
+
     render(){
         const { query } = this.state
         const { books } = this.props
 
-        const showingSearchedBooks = query === '' ? books :
-            books.filter((c) => (
-                c.title.toLowerCase().includes(query.toLowerCase())
-            ));
-        
+        let showingSearchedBooks = this.state.Books;
+
         return(
             <div className="search-books">
                 <div className="search-books-bar">
@@ -45,14 +75,15 @@ class BookSearch extends Component {
                 </div>
                 <div className="search-books-results">
                     <ol className="books-grid">
-                    {showingSearchedBooks.map((book) => (
-                        <li key={book.id}>
+                        { showingSearchedBooks.length > 0  ? showingSearchedBooks.map((book) => (
+                            <li key={book.id}>
                             <SingleBook
-                                book={book}
-                                changeBookFromShelf={this.onChangeBookFromShelf}
+                            book={book}
+                            changeBookFromShelf={this.onChangeBookFromShelf}
                             />
-                        </li>
-                    ))}
+                            </li>
+                            )) : ''
+                        }
                     </ol>
 
                 </div>
